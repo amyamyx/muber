4dd4472ff60b264454310dc2f873262d0b0321ef
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-06 13:38:20 -0700

    Create GET route to get drivers near a location

--- contollers/drivers_controller.js
+++ contollers/drivers_controller.js
@@ -5,6 +5,34 @@ module.exports = {
     res.send({ hi: 'there' });
   },
 
+  // index(req, res, next) {
+  //   res.send({ hi: 'there' });
+  // },
+  index(req, res, next) {
+    const { lng, lat } = req.query;
+
+    // @deprecated function geoNear
+    // Driver.geoNear(
+    //   { type: 'Point', coordinates: [lng, lat] },
+    //   { spherical: true, maxDistance: 200000 }
+    // )
+    //   .then(drivers => res.send(drivers))
+    //   .catch(next);
+    
+    Driver.aggregate([
+      {
+        $geoNear: {
+          near: { type: "Point", coordinates: [parseFloat(lng), parseFloat(lat)] },
+          spherical: true,
+          distanceField: "dist.calculated",
+          maxDistance: 200000
+        }
+      }
+    ])
+      .then(drivers => res.send(drivers))
+      .catch(next);
+  },
+
   create(req, res, next) {
     const driverProps = req.body;
 
